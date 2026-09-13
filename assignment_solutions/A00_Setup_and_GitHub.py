861907a6ab211cfd71dc10ba4f4fdabedb80d4bf
wu1287 <wu1287@mcmaster.ca>
2026-09-12 20:38:26 -0400

    Uploaded .py file for Assignment 0

---
+++ assignment_solutions/A00_Setup_and_GitHub.py
@@ -0,0 +1,9 @@
+# TODO: Edit the strings below, then run the cell.
+first_name = "Jerry"
+last_initial = "W."
+interests = "math, chemistry, photography"
+goals = "learn Python for analysing lab data"
+
+print(f"Hello! I'm {first_name} {last_initial}")
+print("Interests:", interests)
+print("Goals:", goals)
